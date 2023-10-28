99627dbcb6c9708d9d374a1569849ddcca4d1c98
André <lvicenteorellana@gmail.com>
2023-10-27 23:45:50 -0600

    Logica

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.login
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnregistro=findViewById<Button>(R.id.btnregistro)
+        btnregistro.setOnClickListener {
+            val ver = Intent(this, Registro_activity::class.java)
+            startActivity(ver)
+        }
     }
 }
